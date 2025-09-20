ac024144d091140c52d0979c6d068eb28417419d
ILYAS1288 <elibubalteen@gmail.com>
2025-09-20 20:32:34 +0500

    change

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,7 +2,7 @@ import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Main1 } from "./components/Main1";
 import { Main2 } from "./components/Main2";
-
+import { Main3 } from "./components/Main3";
 export function App() {
   return (
     <div>
@@ -11,6 +11,7 @@ export function App() {
     <Main/>
     <Main1/>
     <Main2/>
+    <Main3/>
     </div>
   );
 }
--- my-project/src/components/Main3.jsx
+++ my-project/src/components/Main3.jsx
@@ -1,10 +1,14 @@
-export function Main2() {
+export function Main3() {
     return (
        <main>
 <button> Experience</button>
          <p>Here are some of the companies I've worked with:</p>
-         <h1>T</h1>
+         <h1>TechScape</h1>
+            <p>Role: Frontend Developer</p>
+            <p>Duration: April 2023 - update</p>
+            <p>Responsibilities: Developing and maintaining user interfaces using React and Tailwind CSS. Collaborating with designers and backend developers to create seamless web applications. Implementing responsive design principles to ensure optimal user experience across devices.</p>
+            <p>Achievements: Successfully launched multiple features that improved user engagement by 20%. Optimized application performance, resulting in a 15% reduction in load times.</p>
 
        </main>
        )
-}
+};
